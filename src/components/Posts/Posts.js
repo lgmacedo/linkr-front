e97cf1066cebcc04f892723f03967fa74a085a4f
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-06-02 16:41:45 -0300

    feat: function likePost

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,30 @@
 import styled from "styled-components";
 import { ContainerPost, Description, LeftSidePost, Link, LinkImg, LinkInfo, Name, RightSidePost } from "./styles";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import { useState } from "react";
 
 export default function Posts({post}){
   const { image, userId, username, likescount, likedBy, link, picture, title, description, desc } = post;
+  const { user } = useContext(UserContext);
+  const [like, setLike] = useState('heart-outline')
+  const [color, setColor] = useState('#FFFFFF')
+
+  function likePost(){
+    if(like==='heart-outline'){
+        setLike('heart')
+        setColor('#AC0000')
+    } else{
+        setLike('heart-outline')
+        setColor('#FFFFFF')
+    }
+  }
+
     return (
         <ContainerPost data-test="post">
         <LeftSidePost>
           <img src={picture} alt="profile pic" />
-          <ion-icon name="heart-outline"></ion-icon>
+          <ion-icon name={like} style={{color: color}} onClick={likePost}></ion-icon>
           <p>30 likes</p>
         </LeftSidePost>
         <RightSidePost>
