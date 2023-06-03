935a216fed2259ce55bfe3646c0e0b0d93dbb4a7
aabenjamim <alanebenjamimdossantos@gmail.com>
2023-06-03 19:09:00 -0300

    feat: search users by username

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,16 @@ import { useContext, useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import { DebounceInput } from "react-debounce-input";
+import axios from "axios";
 
 export default function Header() {
-  const {user, setUser} = useContext(UserContext);
+  const { user, setUser, setUserIdSearch } = useContext(UserContext);
   const [menuOpen, setMenuOpen] = useState(false);
   const menuRef = useRef(null);
   const profileRef = useRef(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   const navigate = useNavigate();
 
@@ -34,53 +38,123 @@ export default function Header() {
     setMenuOpen((prevMenuOpen) => !prevMenuOpen);
   };
 
+  const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+  });
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${user.token}`,
+    },
+  };
+
+  function handleSearch(event) {
+    const searchTerm = event.target.value;
+    setSearchTerm(searchTerm);
+    if (searchTerm.length >= 3) {
+      const body = { username: searchTerm };
+      const promise = api.post("/users", body, config)
+      promise
+        .then((res) => {
+          setSearchResults(res.data);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    } else {
+      setSearchResults([]);
+    }
+  }
+
   function logout() {
     localStorage.clear();
     setUser({});
     navigate("/");
   }
 
+  function pageUser(id, username, picture){
+    const obj = { id, username, picture };
+    setUserIdSearch(obj)
+    navigate(`/user/${id}`)
+  }
+
   return (
-  <><Container>
-      <ContainerHeader>
-        <Logo>linkr</Logo>
-        <Visible>
-          <Input>
-            <input placeholder="Search for people" />
-            <ion-icon name="search-sharp"></ion-icon>
-          </Input>
-        </Visible>
-        <ProfileContainer>
-          {menuOpen ? (
-            <SlArrowUp onClick={toggleMenu} />
-          ) : (
-            <SlArrowDown onClick={toggleMenu} />
-          )}
-          <ProfilePicture
-            data-test="avatar"
-            src={user.picture}
-            onClick={toggleMenu}
-            ref={profileRef} />
-          {menuOpen && (
-            <Menu data-test="menu" ref={menuRef}>
-              <button data-test="logout" onClick={logout}>Logout</button>
-            </Menu>
-          )}
-        </ProfileContainer>
-      </ContainerHeader>
-    </Container><Invisible>
+    <>
+      <Container>
+        <ContainerHeader>
+          <Logo>linkr</Logo>
+          <Visible>
+            <Input>
+              <DebounceInput
+                placeholder="Search for people"
+                debounceTimeout={300}
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+              <ion-icon name="search-sharp"></ion-icon>
+            </Input>
+            {searchResults.length > 0 && (
+              <SearchResults>
+                {searchResults.map((result) => (
+                  <SearchResultItem key={result.id} onClick={()=>pageUser(result.id, result.username, result.picture)}>
+                    <img src={result.picture}/>
+                    <p>{result.username}</p>
+                  </SearchResultItem>
+                ))}
+              </SearchResults>
+            )}
+          </Visible>
+          <ProfileContainer>
+            {menuOpen ? (
+              <SlArrowUp onClick={toggleMenu} />
+            ) : (
+              <SlArrowDown onClick={toggleMenu} />
+            )}
+            <ProfilePicture
+              data-test="avatar"
+              src={user.picture}
+              onClick={toggleMenu}
+              ref={profileRef}
+            />
+            {menuOpen && (
+              <Menu data-test="menu" ref={menuRef}>
+                <button data-test="logout" onClick={logout}>
+                  Logout
+                </button>
+              </Menu>
+            )}
+          </ProfileContainer>
+        </ContainerHeader>
+      </Container>
+      <Invisible>
         <InputResponsive>
-          <input placeholder="Search for people" />
+          <DebounceInput
+            placeholder="Search for people"
+            debounceTimeout={300}
+            value={searchTerm}
+            onChange={handleSearch}
+          />
           <ion-icon name="search-sharp"></ion-icon>
         </InputResponsive>
-      </Invisible></>
+        {searchResults.length > 0 && (
+          <SearchResults>
+            {searchResults.map((result) => (
+              <SearchResultItem key={result.id} onClick={()=>pageUser(result.id, result.username, result.picture)}>
+                <img src={result.picture}/>
+                <p>{result.username}</p>
+              </SearchResultItem>
+            ))}
+          </SearchResults>
+        )}
+      </Invisible>
+    </>
   );
 }
 
 const Container = styled.div`
-    height: 100px;
-    position: relative;
-`
+  height: 100px;
+  position: relative;
+`;
 
 const ContainerHeader = styled.div`
   display: flex;
@@ -96,16 +170,14 @@ const ContainerHeader = styled.div`
   z-index: 1;
 `;
 
-
-
 const Logo = styled.h1`
-color: #fff;
-    font-family: 'Passion One', cursive;
-    font-size:49px;
-    letter-spacing: 0.05em;
-    @media (max-width: 420px) {
-        font-size: 45px;
-    }
+  color: #fff;
+  font-family: "Passion One", cursive;
+  font-size: 49px;
+  letter-spacing: 0.05em;
+  @media (max-width: 420px) {
+    font-size: 45px;
+  }
 `;
 
 const ProfileContainer = styled.div`
@@ -124,10 +196,10 @@ const ProfilePicture = styled.img`
   border-radius: 27px;
   object-fit: cover;
   cursor: pointer;
-    @media (max-width: 420px) {
-        width: 41px;
-        height: 41px;
-    }
+  @media (max-width: 420px) {
+    width: 41px;
+    height: 41px;
+  }
 `;
 
 const Menu = styled.div`
@@ -156,88 +228,128 @@ const Menu = styled.div`
 `;
 
 const Input = styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 45px;
-    width: 563px;
-    background-color: #FFFFFF;
-    box-sizing: border-box;
-    align-items: center;
-    padding-left: 14px;
-    padding-right: 14px;
-    border-radius: 8px;
-    input{
-        width: 542px;
-        height: 25px;
-        border: none;
-        outline: none;
-        font-size: 19px;
-        font-family: 'Lato';
-        color: #515151;
-;
-        ::placeholder{
-            color: #C6C6C6;
-            font-size: 19px;
-            font-family: 'Lato';
-        }
+  display: flex;
+  flex-direction: row;
+  height: 45px;
+  width: 563px;
+  background-color: #ffffff;
+  box-sizing: border-box;
+  align-items: center;
+  padding-left: 14px;
+  padding-right: 14px;
+  border-radius: 8px;
+  input {
+    width: 542px;
+    height: 25px;
+    border: none;
+    outline: none;
+    font-size: 19px;
+    font-family: "Lato";
+    color: #515151;
+    ::placeholder {
+      color: #c6c6c6;
+      font-size: 19px;
+      font-family: "Lato";
     }
-    ion-icon{
-        font-size: 21px;
-        color: #C6C6C6;
-        cursor: pointer;
-        :hover{
-            color: #515151;
-        }
+  }
+  ion-icon {
+    font-size: 21px;
+    color: #c6c6c6;
+    cursor: pointer;
+    :hover {
+      color: #515151;
     }
-`
-
-const Visible = styled.div`
-    @media (max-width: 420px) {
-        display: none;
-    }
-`
-const Invisible = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    @media (min-width: 420px) {
-        display: none;
-    }
+  }
 `;
 
+const Visible = styled.div`
+  @media (max-width: 420px) {
+    display: none;
+  }
+`;
+
+const Invisible = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  @media (min-width: 420px) {
+    display: none;
+  }
+`;
 
 const InputResponsive = styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 45px;
+  display: flex;
+  flex-direction: row;
+  height: 45px;
+  width: 350px;
+  background-color: #ffffff;
+  box-sizing: border-box;
+  align-items: center;
+  padding-left: 15px;
+  padding-right: 15px;
+  border-radius: 8px;
+  input {
+    width: 300px;
+    height: 25px;
+    border: none;
+    outline: none;
+    font-size: 18px;
+    font-family: "Lato";
+    color: #515151;
+    ::placeholder {
+      color: #c6c6c6;
+      font-size: 18px;
+      font-family: "Lato";
+    }
+  }
+  ion-icon {
+    font-size: 20px;
+    color: #c6c6c6;
+    cursor: pointer;
+    :hover {
+      color: #515151;
+    }
+  }
+`;
+
+const SearchResults = styled.div`
+  position: absolute;
+  top: 59px;
+  width: 563px;
+  background-color: #E7E7E7;
+  border-radius: 8px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  overflow: auto;
+  max-height: 200px;
+  z-index: 2;
+  @media (max-width: 420px) {
     width: 350px;
-    background-color: #FFFFFF;
-    box-sizing: border-box;
-    align-items: center;
-    padding-left: 15px;
-    padding-right: 15px;
-    border-radius: 8px;
-    input{
-        width: 300px;
-        height: 25px;
-        border: none;
-        outline: none;
-        font-size: 18px;
-        font-family: 'Lato';
-        color: #515151;
-;
-        ::placeholder{
-            color: #C6C6C6;
-            font-size: 18px;
-            font-family: 'Lato';
-        }
-    }
-    ion-icon{
-        font-size: 20px;
-        color: #C6C6C6;
-        cursor: pointer;
-        :hover{
-            color: #515151;
-        }
-    }
-`
+    top: 145px;
+  }
+`;
+
+const SearchResultItem = styled.div`
+  padding: 8px 16px;
+  color: #000;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+  img{
+    width: 39px;
+    height: 39px;
+    border-radius: 85px;
+  }
+  p{
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    line-height: 23px;
+    color: #515151;
+  }
+  &:hover {
+    background-color: #f2f2f2;
+  }
+`;
