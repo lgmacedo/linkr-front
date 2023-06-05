f066db0a875ad8f631ead12c51d2d12c9375c01e
kassioba <kassioluna@gmail.com>
2023-06-05 00:31:30 -0300

    feat: delete posts by postId

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,12 +1,25 @@
-import { ContainerPost, Description, LeftSidePost, Link, LinkImg, LinkInfo, Name, RightSidePost , Hashtag, StyledTooltip} from "./styles";
+import {
+  ContainerPost,
+  Description,
+  LeftSidePost,
+  Link,
+  LinkImg,
+  LinkInfo,
+  Name,
+  RightSidePost,
+  Hashtag,
+  StyledTooltip,
+  ModalContainer,
+} from "./styles";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import reactStringReplace from "react-string-replace";
+import Modal from "react-modal";
 
-export default function Posts({ post }) {
+export default function Posts({ post, getPosts }) {
   const {
     id,
     image,
@@ -24,7 +37,29 @@ export default function Posts({ post }) {
   const [like, setLike] = useState("");
   const [color, setColor] = useState("");
   const [count, setCount] = useState(likescount);
+  const [openModal, setOpenModal] = useState(false);
+  const [modalConfirmText, setModalConfirmText] = useState("Yes, delete it");
   const navigate = useNavigate();
+
+  const modalStyle = {
+    overlay: {
+      backgroundColor: "rgba(255, 255, 255, 0.9)",
+      position: "fixed",
+      zIndex: "2",
+    },
+    content: {
+      height: "262px",
+      width: "597px",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "#333333",
+      color: "#fff",
+      border: "none",
+      borderRadius: "50px",
+      padding: "0px",
+    },
+  };
 
   const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
@@ -78,62 +113,112 @@ export default function Posts({ post }) {
     navigate(`/user/${id}`);
   }
 
+  function deletePost() {
+    setModalConfirmText("Loading...");
+
+    api
+      .delete(`/post/${id}`, config)
+      .then((res) => {
+        getPosts();
+        setOpenModal(false);
+        setModalConfirmText("Yes, delete it");
+      })
+      .catch((err) => {
+        alert("An error occurred while trying to delete the post");
+        setOpenModal(false);
+        setModalConfirmText("Yes, delete it");
+      });
+  }
+
   return (
-    <ContainerPost data-test="post">
-      <LeftSidePost>
-        <img
-          src={picture}
-          alt="profile pic"
-          onClick={() => searchUserId(userId)}
-        />
-        <ion-icon
-          name={like}
-          style={{ color: color }}
-          onClick={likePost}
-        ></ion-icon>
-        <p
-          data-tooltip-content={
-            likedBy && (
-              likedBy.length === 1 ? 
-                (likedBy[0].name === user.username ? "You liked" : `${likedBy[0].name} liked`) :
-              likedBy.length === 2 ? 
-                (likedBy.some((obj) => obj.name === user.username) ?
-                  `You and ${likedBy.find((obj) => obj.name !== user.username)?.name} liked` :
-                  likedBy.map((obj) => obj.name).join(" and ") + " liked") :
-              likedBy.some((obj) => obj.name === user.username) ?
-                (`You, ${likedBy.find((obj) => obj.name !== user.username)?.name} and other ${likedBy.length - 2} people liked`) :
-                `${likedBy[0].name}, ${likedBy[1].name} and other ${likedBy.length - 2} people liked`
-            )
-          }
-          data-tooltip-id={`tooltip-${id}`}
-        >
-          {count} likes
-        </p>
-        <StyledTooltip id={`tooltip-${id}`} place="bottom" effect="solid">
-        </StyledTooltip>
-      </LeftSidePost>
-      <RightSidePost>
-        <Name data-test="username" onClick={() => searchUserId(userId)}>
-          {username}
-        </Name>
-        <Description data-test="description">
-          {reactStringReplace(description, /#(\w+)/g, (match, i) => (
-            <Hashtag
-              onClick={() => navigate(`/hashtag/${match.replace("#", "")}`)}
-            >
-              #{match}
-            </Hashtag>
-          ))}
-        </Description>
-        <Link data-test="link" href={link} target="_blank">
-          <LinkInfo>
-            <p className="title">{title}</p>
-            <p className="desc">{desc}</p>
-            <p className="url">{link}</p>
-          </LinkInfo>
-          <LinkImg src={image} alt={title} />
-        </Link>
-      </RightSidePost>
-    </ContainerPost>
+    <>
+      <Modal isOpen={openModal} style={modalStyle}>
+        <ModalContainer openModal={openModal}>
+          <div className="container">
+            <span className="text">
+              Are you sure you want to delete this post?
+            </span>
+            <div className="button-container">
+              <button onClick={() => setOpenModal(false)} className="no">
+                No, go back
+              </button>
+              <button className="yes" onClick={deletePost}>
+                {modalConfirmText}
+              </button>
+            </div>
+          </div>
+        </ModalContainer>
+      </Modal>
+
+      <ContainerPost data-test="post">
+        <div className="delete" onClick={() => setOpenModal(true)}>
+          <ion-icon name="trash" />
+        </div>
+        <LeftSidePost>
+          <img
+            src={picture}
+            alt="profile pic"
+            onClick={() => searchUserId(userId)}
+          />
+          <ion-icon
+            name={like}
+            style={{ color: color }}
+            onClick={likePost}
+          ></ion-icon>
+          <p
+            data-tooltip-content={
+              likedBy &&
+              (likedBy.length === 1
+                ? likedBy[0].name === user.username
+                  ? "You liked"
+                  : `${likedBy[0].name} liked`
+                : likedBy.length === 2
+                ? likedBy.some((obj) => obj.name === user.username)
+                  ? `You and ${
+                      likedBy.find((obj) => obj.name !== user.username)?.name
+                    } liked`
+                  : likedBy.map((obj) => obj.name).join(" and ") + " liked"
+                : likedBy.some((obj) => obj.name === user.username)
+                ? `You, ${
+                    likedBy.find((obj) => obj.name !== user.username)?.name
+                  } and other ${likedBy.length - 2} people liked`
+                : `${likedBy[0].name}, ${likedBy[1].name} and other ${
+                    likedBy.length - 2
+                  } people liked`)
+            }
+            data-tooltip-id={`tooltip-${id}`}
+          >
+            {count} likes
+          </p>
+          <StyledTooltip
+            id={`tooltip-${id}`}
+            place="bottom"
+            effect="solid"
+          ></StyledTooltip>
+        </LeftSidePost>
+        <RightSidePost>
+          <Name data-test="username" onClick={() => searchUserId(userId)}>
+            {username}
+          </Name>
+          <Description data-test="description">
+            {reactStringReplace(description, /#(\w+)/g, (match, i) => (
+              <Hashtag
+                onClick={() => navigate(`/hashtag/${match.replace("#", "")}`)}
+              >
+                #{match}
+              </Hashtag>
+            ))}
+          </Description>
+          <Link data-test="link" href={link} target="_blank">
+            <LinkInfo>
+              <p className="title">{title}</p>
+              <p className="desc">{desc}</p>
+              <p className="url">{link}</p>
+            </LinkInfo>
+            <LinkImg src={image} alt={title} />
+          </Link>
+        </RightSidePost>
+      </ContainerPost>
+    </>
   );
 }
--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -16,134 +16,145 @@ export const StyledTooltip = styled(Tooltip)`
 `;
 
 export const ContainerPost = styled.div`
-    width: 611px;
-    min-height:276px;
-    background-color: #171717;
-    border-radius: 16px;
-    display: flex;
-    margin-bottom: 10px;
-    padding:5px 5px 10px;
-    gap:5px;
-    @media (max-width: 420px) {
-      width:100%;
-      min-height:232px;
-      border-radius: 0;
-    }
-`
+  width: 611px;
+  min-height: 276px;
+  background-color: #171717;
+  border-radius: 16px;
+  display: flex;
+  margin-bottom: 10px;
+  padding: 5px 5px 10px;
+  gap: 5px;
+  position: relative;
+
+  .delete {
+    position: absolute;
+    color: #fff;
+    font-size: 14px;
+    right: 22px;
+    top: 23px;
+    cursor: pointer;
+  }
+
+  @media (max-width: 420px) {
+    width: 100%;
+    min-height: 232px;
+    border-radius: 0;
+  }
+`;
 
 export const LeftSidePost = styled.div`
-display:flex;
-padding:20px 0 20px 20px;
-flex-direction: column;
-align-items: center;
-img {
-  width:50px;
-  height:50px;
-  border-radius: 27px;
-  object-fit: cover;
-  cursor: pointer;
-}
-ion-icon{
-  color: #FFFFFF;
-  font-size: 20px;
-  margin-top: 10px;
-  cursor: pointer;
-}
-p{
-  font-family: 'Lato';
-  font-weight: 400;
-  font-size: 11px;
-  line-height: 13px;
-  text-align: center;
-  margin-top: 5px;
-  color: #FFFFFF;
-}
-@media (max-width: 420px) {
+  display: flex;
+  padding: 20px 0 20px 20px;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 27px;
+    object-fit: cover;
+    cursor: pointer;
+  }
+  ion-icon {
+    color: #ffffff;
+    font-size: 20px;
+    margin-top: 10px;
+    cursor: pointer;
+  }
+  p {
+    font-family: "Lato";
+    font-weight: 400;
+    font-size: 11px;
+    line-height: 13px;
+    text-align: center;
+    margin-top: 5px;
+    color: #ffffff;
+  }
+  @media (max-width: 420px) {
     img {
-        width:40px;
-        height:40px;
+      width: 40px;
+      height: 40px;
     }
   }
-`
+`;
 
 export const RightSidePost = styled.div`
-    display:flex;
-    flex-direction:column;
-    padding:10px;
-    gap:10px;
-    width:100%;
-    p {
-        font-family: 'Lato', sans-serif;
-        font-weight:400;
-    }
-`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  gap: 10px;
+  width: 100%;
+  p {
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+  }
+`;
 
 export const Name = styled.p`
-    margin: 15px 0 0px 0;
-    font-size:19px;
-    line-height: 23px;
-    color: #fff;
-    cursor: pointer;
-`
+  margin: 15px 0 0px 0;
+  font-size: 19px;
+  line-height: 23px;
+  color: #fff;
+  cursor: pointer;
+`;
 
 export const Description = styled.p`
-    font-size:17px;
-    line-height: 20px;
-    color:#b7b7b7;
-`
+  font-size: 17px;
+  line-height: 20px;
+  color: #b7b7b7;
+`;
 
 export const Link = styled.a`
-    display:flex;
-    align-items: center;
-    width:100%;
-    height:155px;
-    border: 1px solid #4d4d4d;
-    border-radius: 11px;
-    cursor:pointer;
-    width: 100%;
-    @media (max-width: 420px) {
-     width:90%;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 155px;
+  border: 1px solid #4d4d4d;
+  border-radius: 11px;
+  cursor: pointer;
+  width: 100%;
+  @media (max-width: 420px) {
+    width: 90%;
   }
 `;
 
 export const LinkInfo = styled.div`
-display: flex;
-flex-direction: column;
-padding:20px 0 25px 25px;
-gap:10px;
-width:70%;
-p {
-    font-family: 'Lato', sans-serif;
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0 25px 25px;
+  gap: 10px;
+  width: 70%;
+  p {
+    font-family: "Lato", sans-serif;
     font-weight: 400;
-    overflow:hidden;
+    overflow: hidden;
     display: -webkit-box;
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
     overflow-wrap: anywhere;
-}
-.title {
+  }
+  .title {
     font-weight: 400;
-    color:#cecece;
-    font-size:16px;
+    color: #cecece;
+    font-size: 16px;
     line-height: 19px;
-    height:37px;
-}
-.desc {
+    height: 37px;
+  }
+  .desc {
     font-weight: 400;
-    color:#9B9595;
-    font-size:11px;
+    color: #9b9595;
+    font-size: 11px;
     line-height: 13px;
-    height:39px;
-}
-.url {
+    height: 39px;
+  }
+  .url {
     font-weight: 400;
-    color:#CECECE;
-    font-size:11px;
+    color: #cecece;
+    font-size: 11px;
     line-height: 13px;
-    height:25px;
-}
-@media (max-width: 611px) {
-    height:100%;
+    height: 25px;
+  }
+  @media (max-width: 611px) {
+    height: 100%;
     .title {
       font-size: 11px;
       line-height: 13px;
@@ -157,18 +168,18 @@ p {
       line-height: 11px;
     }
   }
-`
+`;
 
 export const LinkImg = styled.img`
-    width:155px;
-    height:155px;
-    object-fit: cover;
-    border-radius: 0px 12px 13px 0px;
-    @media (max-width: 420px) {
-     width:30%;
-     height:100%;
+  width: 155px;
+  height: 155px;
+  object-fit: cover;
+  border-radius: 0px 12px 13px 0px;
+  @media (max-width: 420px) {
+    width: 30%;
+    height: 100%;
   }
-`
+`;
 
 export const Hashtag = styled.span`
   font-weight: 900;
@@ -176,5 +187,55 @@ export const Hashtag = styled.span`
 
   :hover {
     text-decoration: underline;
+  }
+`;
+
+export const ModalContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 38px;
+  box-sizing: border-box;
+
+  .container {
+    width: 370px;
+    height: 159px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    font-family: "Lato", sans-serif;
+  }
+
+  .text {
+    font-size: 34px;
+    font-weight: 700;
+    text-align: center;
+  }
+
+  .button-container {
+    width: 295px;
+    height: fit-content;
+    display: flex;
+    justify-content: space-between;
+
+    button {
+      height: 37px;
+      width: 137px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      font-size: 18px;
+      font-weight: 700;
+    }
+
+    .yes {
+      background-color: #1877f2;
+      color: #fff;
+    }
+
+    .no {
+      background-color: #fff;
+      color: #1877f2;
+    }
   }
 `;
